5b2c50fbe98e8cdb5693dda5ce6e67c267d46119
Nguyễn Văn Khánh <57382524+COCC390@users.noreply.github.com>
2022-08-19 10:17:45 +0700

    small update
    - change main character model
    - add new input system
    - add animation for main character

---
+++ MonsterAge/Assets/Scripts/Main Character/ControlManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ControlManager : MonoBehaviour
+{
+    #region public variable
+    #endregion
+
+    #region private variable
+
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float jumpForce;
+
+    [SerializeField] private Rigidbody rb;
+
+    #endregion
+
+    #region Unity Method
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    #endregion
+
+    #region Custom Method
+
+    private void Move()
+    {
+        
+    }
+
+    private void Jump()
+    {
+
+    }    
+
+    #endregion
+}
